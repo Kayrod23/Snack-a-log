6d3cd2827068363cc85627996ab31c3f5c99b527
Keeanu Rodriguez <keeanu7@gmail.com>
2023-05-10 12:19:40 -0400

    all routes added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import { Route, BrowerserRouter, Routes } from "react-router-dom"
+import Home from "./components/Home.jsx"
+import Error from "./components/Error.jsx"
+import Index from "./components/Index.jsx"
+import New from "./components/New.jsx"
+import Show from "./components/Show.jsx"
+import Edit from "./components/Edit.jsx"
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowerserRouter>
+    <Routes>
+    <Route path="*" element={<Error/>}/> 
+      <Route path="/" element={<Home/>}/>
+      <Route path="/snacks" element={<Index/>}/>
+      <Route path="/snacks/new" element={<New/>}/>
+      <Route path="/snacks/:id" element={<Show/>}/>
+      <Route path="/snacks/:id/edit" element={<Edit/>}/>
+    </Routes>
+    </BrowerserRouter>
   );
 }
 
